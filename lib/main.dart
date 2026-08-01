66bf81063a4935dc220aaf01284e53c9ade75606
LucasBruz <lucasbruz1993@gmail.com>
2026-08-01 14:04:01 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 
-// TODO: descomentar cuando se agregue firebase_options.dart generado
-// por `flutterfire configure`
-// import 'firebase_options.dart';
-// import 'package:firebase_core/firebase_core.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('es'); // fechas del calendario en español
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await initializeDateFormatting('es');
   runApp(const ProviderScope(child: MyBandApp()));
 }
 
@@ -29,7 +24,11 @@ class MyBandApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       locale: const Locale('es'),
       supportedLocales: const [Locale('es'), Locale('en')],
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       routerConfig: appRouter,
     );
   }
